20fb48f181ff5dba05cc97527cf6cb1e494ea2d0
Kevin Neilson <kevin@papermoon.io>
2024-08-02 15:33:22 -0700

    set up blankish branch

--- .snippets/code/build/build-multichain-applications/wormhole-sdk/example-request-initiate.ts
+++
@@ -1,4 +0,0 @@
-// Now the transfer may be initiated
-// A receipt will be returned, guess what you gotta do with that?
-const receipt = await bestRoute.initiate(sender.signer, quote);
-console.log('Initiated transfer with receipt: ', receipt);
