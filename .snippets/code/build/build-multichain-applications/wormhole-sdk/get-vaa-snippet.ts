f36ba16a066038a7d1d0f4e0a696b5abf5e0610e
Kevin Neilson <kevin@papermoon.io>
2024-08-02 17:39:33 -0700

    rev

--- .snippets/code/build/build-multichain-applications/wormhole-sdk/get-vaa-snippet.ts
+++
@@ -1,9 +0,0 @@
-// Get the VAA from the wormhole message id
-const vaa = await wh.getVaa(
-  // Wormhole Message ID
-  whm!,
-  // Protocol:Payload name to use for decoding the VAA payload
-  'TokenBridge:Transfer',
-  // Timeout in milliseconds, depending on the chain and network, the VAA may take some time to be available
-  60_000
-);
