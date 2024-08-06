f64696815a8d3eec23c43831e3c8a88a0f07ed55
Kevin Neilson <kevin@papermoon.io>
2024-08-05 18:41:39 -0700

    rev

--- .snippets/code/build/build-multichain-applications/queries/getting-started/query-request-with-api-key.jsx
+++
@@ -1,9 +0,0 @@
-const serialized = request.serialize();
-const proxyResponse =
-  (await axios.post) <
-  QueryProxyQueryResponse >
-  (QUERY_URL,
-  {
-    bytes: Buffer.from(serialized).toString("hex"),
-  },
-  { headers: { "X-API-Key": YOUR_API_KEY } });
--- .snippets/code/build/build-multichain-applications/queries/getting-started/query-request.jsx
+++
@@ -1,9 +0,0 @@
-const request = new QueryRequest(
-  0, // nonce
-  [
-    new PerChainQueryRequest(
-      2, // Ethereum Wormhole Chain ID
-      new EthCallQueryRequest(latestBlock, [callData])
-    ),
-  ]
-);
