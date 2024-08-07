a3ae6839544cdca5b2fd1e6f192bced68410923a
Kevin Neilson <kevin@papermoon.io>
2024-08-06 19:17:37 -0700

    run prettier

--- .snippets/code/build/build-multichain-applications/connect/configuration/configure-react.tsx
+++ .snippets/code/build/build-multichain-applications/connect/configuration/configure-react.tsx
@@ -1,11 +1,13 @@
-import WormholeConnect, { WormholeConnectConfig } from '@wormhole-foundation/wormhole-connect';
+import WormholeConnect, {
+  WormholeConnectConfig,
+} from '@wormhole-foundation/wormhole-connect';
 
 const config: WormholeConnectConfig = {
-  networks: ["ethereum", "polygon", "solana"],
-  tokens: ["ETH", "WETH", "MATIC", "WMATIC"],
+  networks: ['ethereum', 'polygon', 'solana'],
+  tokens: ['ETH', 'WETH', 'MATIC', 'WMATIC'],
   rpcs: {
-    ethereum: "https://rpc.ankr.com/eth",
-    solana: "https://rpc.ankr.com/solana",
+    ethereum: 'https://rpc.ankr.com/eth',
+    solana: 'https://rpc.ankr.com/solana',
   }
 }
 
