ecd23272f112622956de28b152e0d3a1e63b961e
Kevin Neilson <kevin@papermoon.io>
2024-08-08 15:19:48 -0700

    prettier

--- .snippets/code/build/build-multichain-applications/queries/getting-started/query-request-with-api-key.jsx
+++ .snippets/code/build/build-multichain-applications/queries/getting-started/query-request-with-api-key.jsx
@@ -1,9 +1,8 @@
 const serialized = request.serialize();
-const proxyResponse =
-  (await axios.post) <
-  QueryProxyQueryResponse >
-  (QUERY_URL,
+const proxyResponse = (await axios.post)<QueryProxyQueryResponse>(
+  QUERY_URL,
   {
-    bytes: Buffer.from(serialized).toString("hex"),
+    bytes: Buffer.from(serialized).toString('hex'),
   },
-  { headers: { "X-API-Key": YOUR_API_KEY } });
+  { headers: { 'X-API-Key': YOUR_API_KEY } }
+);
--- .snippets/code/build/build-multichain-applications/queries/getting-started/test-full.jsx
+++ .snippets/code/build/build-multichain-applications/queries/getting-started/test-full.jsx
@@ -6,41 +6,41 @@ import {
   QueryProxyMock,
   QueryRequest,
   QueryResponse,
-} from "@wormhole-foundation/wormhole-query-sdk";
-import axios from "axios";
+} from '@wormhole-foundation/wormhole-query-sdk';
+import axios from 'axios';
 
-const rpc = "https://ethereum.publicnode.com";
+const rpc = 'https://ethereum.publicnode.com';
 const callData: EthCallData = {
-  to: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
-  data: "0x18160ddd", // web3.eth.abi.encodeFunctionSignature("totalSupply()")
+  to: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
+  data: '0x18160ddd', // web3.eth.abi.encodeFunctionSignature("totalSupply()")
 };
 
 (async () => {
   const latestBlock: string = (
     await axios.post(rpc, {
-      method: "eth_getBlockByNumber",
-      params: ["latest", false],
+      method: 'eth_getBlockByNumber',
+      params: ['latest', false],
       id: 1,
-      jsonrpc: "2.0",
+      jsonrpc: '2.0',
     })
   ).data?.result?.number;
   if (!latestBlock) {
     console.error(`❌ Invalid block returned`);
     return;
   }
-  console.log("Latest Block:     ", latestBlock, `(${BigInt(latestBlock)})`);
+  console.log('Latest Block:     ', latestBlock, `(${BigInt(latestBlock)})`);
   const targetResponse = await axios.post(rpc, {
-    method: "eth_call",
+    method: 'eth_call',
     params: [callData, latestBlock],
     id: 1,
-    jsonrpc: "2.0",
+    jsonrpc: '2.0',
   });
   // console.log(finalizedResponse.data);
   if (targetResponse.data.error) {
     console.error(`❌ ${targetResponse.data.error.message}`);
   }
   const targetResult = targetResponse.data?.result;
-  console.log("Target Result:    ", targetResult, `(${BigInt(targetResult)})`);
+  console.log('Target Result:    ', targetResult, `(${BigInt(targetResult)})`);
   // form the query request
   const request = new QueryRequest(
     0, // nonce
