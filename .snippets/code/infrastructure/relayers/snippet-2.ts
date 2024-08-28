48d9f8da7659e3bab727fd302c32b0cbde9b3a25
martin0995 <martin@papermoon.io>
2024-08-28 17:29:51 +0200

    relayers will be added in another PR

--- .snippets/code/infrastructure/relayers/snippet-2.ts
+++
@@ -1,5 +0,0 @@
-export class StandardRelayerApp<
-  ContextT extends StandardRelayerContext = StandardRelayerContext,
-> extends RelayerApp<ContextT> {
-  // ...
-  constructor(env: Environment, opts: StandardRelayerAppOpts) {
