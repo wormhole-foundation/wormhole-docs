2f2ae26d683e9980ed673d71beb8b0224f8833ef
martin0995 <martin@papermoon.io>
2024-09-11 17:58:10 +0200

    added main snippets for cctp

--- .snippets/code/tutorials/messaging/cctp/snippet-3.ts
+++ .snippets/code/tutorials/messaging/cctp/snippet-3.ts
@@ -2,12 +2,13 @@ import { wormhole } from '@wormhole-foundation/sdk';
 import evm from '@wormhole-foundation/sdk/evm';
 import solana from '@wormhole-foundation/sdk/solana';
 import * as dotenv from 'dotenv';
-import { getSigner } from './helpers/helpers';
+import { getSigner } from '../helpers/helpers';
 
 // Load environment variables
 dotenv.config();
 
 (async function () {
+  // Initialize the Wormhole object for the Testnet environment and add supported chains (evm and solana)
   const wh = await wormhole('Testnet', [evm, solana]);
 
   // Grab chain Contexts -- these hold a reference to a cached rpc client
@@ -21,7 +22,7 @@ dotenv.config();
   // Define the amount of USDC to transfer (in the smallest unit, so 0.1 USDC = 100,000 units assuming 6 decimals)
   const amt = 100_000_000n;
 
-  const automatic = false;
+  const automatic = true;
 
   // Create the circleTransfer transaction (USDC-only)
   const xfer = await wh.circleTransfer(
@@ -37,19 +38,6 @@ dotenv.config();
   console.log('Starting Transfer');
   const srcTxids = await xfer.initiateTransfer(source.signer);
   console.log(`Started Transfer: `, srcTxids);
-
-  // Step 2: Wait for Circle Attestation (VAA)
-  const timeout = 60 * 1000; // Timeout in milliseconds (60 seconds)
-  console.log('Waiting for Attestation');
-  const attestIds = await xfer.fetchAttestation(timeout);
-  console.log(`Got Attestation: `, attestIds);
-
-  // Step 3: Complete the transfer on the destination chain (Celo)
-  console.log('Completing Transfer');
-  const dstTxids = await xfer.completeTransfer(destination.signer);
-  console.log(`Completed Transfer: `, dstTxids);
-
-  console.log('Circle Transfer status: ', xfer);
 
   process.exit(0);
 })();
