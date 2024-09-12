bac224b70f66e98173a1443dd629aeca7cc70459
martin0995 <martin@papermoon.io>
2024-09-12 15:49:31 +0200

    updated amount from snippet

--- .snippets/code/tutorials/messaging/cctp/snippet-1.ts
+++ .snippets/code/tutorials/messaging/cctp/snippet-1.ts
@@ -19,7 +19,7 @@ dotenv.config();
   const destination = await getSigner(rcvChain);
 
   // Define the amount of USDC to transfer (in the smallest unit, so 0.1 USDC = 100,000 units assuming 6 decimals)
-  const amt = 100_000_000n;
+  const amt = 100_000n;
 
   const automatic = false;
 
