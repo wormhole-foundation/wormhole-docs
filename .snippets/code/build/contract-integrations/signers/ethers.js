52fe824d4a9ef831a5df504e9c27961b20af6c2f
martin0995 <martin@papermoon.io>
2024-10-01 17:34:17 +0200

    updated ethers snippet

--- .snippets/code/build/contract-integrations/signers/ethers.js
+++ .snippets/code/build/contract-integrations/signers/ethers.js
@@ -1,19 +1,21 @@
 import { ethers } from 'ethers';
 
-// Set up a provider and signer (replace with your own provider URL)
-const provider = new ethers.providers.JsonRpcProvider(
-  'https://your-rpc-provider-url'
-);
-const privateKey = 'your-private-key';
+// Update the following variables
+const rpcUrl = 'INSERT_RPC_URL';
+const privateKey = 'INSERT_PRIVATE_KEY';
+const toAddress = 'INSERT_RECIPIENT_ADDRESS';
+
+// Set up a provider and signer
+const provider = new ethers.JsonRpcProvider(rpcUrl);
 const signer = new ethers.Wallet(privateKey, provider);
 
 // Example: Signing and sending a transaction
 async function sendTransaction() {
   const tx = {
-    to: 'recipient-address',
-    value: ethers.utils.parseEther('0.1'), // Sending 0.1 ETH
+    to: toAddress,
+    value: ethers.parseUnits('0.1'), // Sending 0.1 ETH
     gasPrice: await provider.getGasPrice(),
-    gasLimit: ethers.utils.hexlify(21000),
+    gasLimit: ethers.toBeHex(21000),
   };
 
   const transaction = await signer.sendTransaction(tx);
