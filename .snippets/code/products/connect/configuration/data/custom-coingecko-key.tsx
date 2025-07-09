02ff59662c35ef27a08d9f7443eb593eb9c1e3e6
martin0995 <martin@papermoon.io>
2025-07-09 10:56:27 +0200

    remove coingecko file

--- .snippets/code/products/connect/configuration/data/custom-coingecko-key.tsx
+++
@@ -1,9 +0,0 @@
-import WormholeConnect, { type config } from '@wormhole-foundation/wormhole-connect';
-
-const config: config.WormholeConnectConfig = {
-  coinGeckoApiKey: 'INSERT_API_KEY',
-};
-
-function App() {
-  return <WormholeConnect config={config} />;
-}
